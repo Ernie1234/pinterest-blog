cf3b7cef05f0d790fee2373659637b71e5743eee
unknown <anumahjoshuaeneye@gmail.com>
2024-03-01 01:02:04 +0100

    btn fix

--- components/LinkBtn.tsx
+++ components/LinkBtn.tsx
@@ -6,7 +6,7 @@ function LinkBtn({ link, title }: { link: string; title: string }) {
   return (
     <Link
       href={link}
-      className="mt-3 p-5 text-center text-black capitalize font-bold border-2 border-black items-center rounded-full cursor-pointer transform hover:scale-x-110 hover:scale-y-105 transition duration-300 ease-out"
+      className="mt-3 p-5 text-center text-black capitalize font-bold border-2 border-black items-center rounded-full cursor-pointer hover:bg-black hover:text-white hover:scale-x-110 hover:scale-y-105 transition-all duration-300 ease-out"
     >
       {title}
     </Link>

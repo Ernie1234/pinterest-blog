b6b19585ad8e67215bd8480ad694595be779c80d
unknown <anumahjoshuaeneye@gmail.com>
2024-03-09 17:01:42 +0100

    single blog done

--- app/news/[newId]/page.tsx
+++ app/news/[newId]/page.tsx
@@ -1,6 +1,10 @@
 import { IPost } from "@/constants/types";
 import { NEWS_DETAIL } from "@/sanity/lib/queries";
+import Image from "next/image";
+
 import { loadQuery } from "@/sanity/lib/store";
+import { PortableText } from "@portabletext/react";
+import { RichText } from "@/components/RictText";
 
 interface Props {
   params: {
@@ -16,14 +20,28 @@ async function page({ params }: Props) {
 
   return (
     <div>
-      <div className="flex flex-col justify-center items-center mx-auto min-h-screen w-full max-w-screen-lg bg-red-400">
-        <h1 className="lg:pt-32 lg:text-6xl md:text-4xl text-3xl font-semibold text-slate-900">
+      <div className="flex flex-col justify-center items-center mx-auto min-h-screen w-full max-w-screen-lg">
+        <h1 className="text-center lg:pt-32 lg:text-6xl md:text-4xl text-3xl font-semibold text-slate-900">
           {item.title}
         </h1>
-        <p className="group-hover:underline text-2xl text-center pt-6 px-1">
+        <p className="group-hover:underline text-2xl text-center py-6 md:py-12 lg:py-20 px-1">
           21 February 2024
         </p>
-        <div className="flex-1"></div>
+        <div className="">
+          <Image
+            src={item.mainImage.asset.url}
+            alt="news-image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            objectFit="cover"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+        <div className="flex flex-col items-center justify-center">
+          <PortableText value={item?.body} components={RichText} />
+        </div>
+        {/* <div className="flex-1"></div> */}
       </div>
     </div>
   );
---
+++ components/RictText.tsx
@@ -0,0 +1,66 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { urlFor } from "@/sanity/lib/createClient";
+
+export const RichText = {
+  types: {
+    image: ({ value }: any) => {
+      return (
+        <div className="flex items-center justify-center py-6 md:py-12">
+          <Image
+            src={urlFor(value).url()}
+            alt="Post image"
+            width={0}
+            height={0}
+            sizes="100vw"
+            objectFit="cover"
+            style={{ width: "100%", height: "100%" }}
+          />
+        </div>
+      );
+    },
+  },
+  list: {
+    bullet: ({ children }: any) => (
+      <ul className="ml-10 py-5 list-disc space-y-5">{children}</ul>
+    ),
+  },
+  number: ({ children }: any) => (
+    <ol className="mt-lg list-decimal">{children}</ol>
+  ),
+  block: {
+    h1: ({ children }: any) => (
+      <h1 className="text-4xl py-10 font-bold">{children}</h1>
+    ),
+    h2: ({ children }: any) => (
+      <h2 className="text-3xl py-10 font-bold">{children}</h2>
+    ),
+    h3: ({ children }: any) => (
+      <h3 className="text-2xl py-10 font-bold">{children}</h3>
+    ),
+    h4: ({ children }: any) => (
+      <h4 className="text-4xl py-10 font-bold">{children}</h4>
+    ),
+    p: ({ children }: any) => (
+      <p className="text-6xl py-10 font-bold">{children}</p>
+    ),
+    normal: ({ children }: any) => (
+      <p className="text-xl max-w-prose py-10 ">{children}</p>
+    ),
+    blockquote: ({ children }: any) => (
+      <blockquote className="border-l-blue-600 border-l-4 pl-5 py-5 my-5">
+        {children}
+      </blockquote>
+    ),
+  },
+  marks: {
+    link: ({ children, value }: any) => {
+      return (
+        <Link href={value.href} target="_blank" className="underline">
+          {children}
+        </Link>
+      );
+    },
+  },
+};
